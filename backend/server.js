35dbeffe7c565855707d7eff281879ec3583f65b
sj7300834 <sj7300834@gmail.com>
2026-03-28 00:28:20 +0530

    login fix

--- backend/server.js
+++ backend/server.js
@@ -70,6 +70,8 @@ const allowedOrigins = [
   process.env.FRONTEND_URL,
   "http://localhost:3000",
   "http://127.0.0.1:3000",
+   "http://localhost:5173",
+  "http://127.0.0.1:5173",
 ];
 
 app.use(
